b0d666009584ca38abbccf347e24b0a785ea1471
shawshark <chrissbolding@gmail.com>
2014-01-15 01:08:54 +1100

    Create send.java

---
+++ me/shawshark/lilygamesigns/send.java
@@ -0,0 +1,40 @@
+package me.shawshark.lilygamesigns;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Collections;
+
+import lilypad.client.connect.api.request.RequestException;
+import lilypad.client.connect.api.request.impl.MessageRequest;
+
+import org.bukkit.Bukkit;
+
+public class send {
+	
+    public LilyGameSigns lgs;
+    
+	public send(LilyGameSigns lgs) {
+		this.lgs = lgs;
+	}
+	
+    public void sendinfo(String status) {
+	 	
+    	String serverusername = lgs.connect.getSettings().getUsername();
+        String onlineusers = Bukkit.getOnlinePlayers().length + "";
+        String maxusers = Bukkit.getMaxPlayers() + "";
+        
+        try {
+        	MessageRequest request = new 
+        			MessageRequest(Collections.<String> emptyList(), "LilyGameSigns",
+        					serverusername 
+        					+ ",," + 
+        					onlineusers 
+        					+ ",," + 
+        					maxusers 
+        					+ ",," + 
+        					status);
+              lgs.connect.request(request); 
+              } catch (UnsupportedEncodingException | RequestException e){e.printStackTrace();
+        } 
+	}
+
+}
